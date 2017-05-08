e6cb0c47fdd4921633d67fcf2850a28534b04cd8
Staci McWilliams <stacinicole86@gmail.com>
2017-05-08 09:46:52 -0600

    Update database connection

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   test: {
    client: 'pg',
-   connection: 'postgres://localhost/jetfuel_test',
+   connection: process.env.DATABASE_URL || 'postgres://localhost/jetfueltest',
    migrations: {
      directory: __dirname + '/db/migrations'
    },
