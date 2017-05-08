4766dafde051673eee697a4990d197e3b1e02107
Franklin Crosby <obleo33@gmail.com>
2017-05-08 10:06:43 -0600

    Add staging env

--- knexfile.js
+++ knexfile.js
@@ -24,19 +24,12 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   },
 
   production: {
